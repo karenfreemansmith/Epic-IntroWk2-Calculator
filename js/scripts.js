2d489507b5c3fca0e461a18ae7ee3b15bccb32da
Brenda Farrier and Karen Freeman-Smith <brenda@>
2016-08-08 13:24:42 -0700

    added * and /

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,41 @@
 var add = function(number1, number2) {
   return number1 + number2;
 };
-alert(add(10,5));
+
 
 var number1 = parseInt (prompt("Enter a number:"));
 var number2 = parseInt (prompt("Enter another number:"));
 
 alert(add(number1, number2));
+
+
+
+
+var substract = function(number1, number2) {
+  return number1 - number2;
+};
+
+
+var number1 = parseInt (prompt("Enter a number:"));
+var number2 = parseInt (prompt("Enter another number:"));
+
+alert(substract(number1, number2));
+
+var multiply = function(number1, number2) {
+  return number1 * number2;
+};
+
+
+var number1 = parseInt (prompt("Enter a number:"));
+var number2 = parseInt (prompt("Enter another number:"));
+
+alert(multiply(number1, number2));
+
+var division = function(number1, number2) {
+  return number1 / number2;
+}
+
+var number1 = parseInt (prompt("Enter a number"));
+var number2 = parseInt (prompt ("Enter another number"));
+
+alert (division(number1, number2));
