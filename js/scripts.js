6cd8725eb1768555369b7e9b82425bec9b82b812
Adam Gorbahn and Karen Freeman-Smith <adam+karen@>
2016-08-10 11:03:49 -0700

    add javascript functions for new forms

--- js/scripts.js
+++ js/scripts.js
@@ -22,4 +22,19 @@ $(document).ready(function() {
     var result = add(parseInt($("#add1").val()), parseInt($("#add2").val()));
     $("#output").text(result);
   });
+  $("form#sub").submit(function(event) {
+    event.preventDefault();
+    var result = subtract(parseInt($("#sub1").val()), parseInt($("#sub2").val()));
+    $("#output").text(result);
+  });
+  $("form#mul").submit(function(event) {
+    event.preventDefault();
+    var result = multiply(parseInt($("#mul1").val()), parseInt($("#mul2").val()));
+    $("#output").text(result);
+  });
+  $("form#div").submit(function(event) {
+    event.preventDefault();
+    var result = divide(parseInt($("#div1").val()), parseInt($("#div2").val()));
+    $("#output").text(result);
+  });
 });
