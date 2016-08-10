a90b0b621f72f2ea851082726cfcd5ad88f35c4f
Adam Gorbahn and Karen Freeman-Smith <karen@>
2016-08-10 10:47:36 -0700

    update the calculator with a user interface.

--- js/scripts.js
+++ js/scripts.js
@@ -1,41 +1,25 @@
+//business logic
 var add = function(number1, number2) {
   return number1 + number2;
 };
 
-
-var number1 = parseInt (prompt("Enter a number:"));
-var number2 = parseInt (prompt("Enter another number:"));
-
-alert(add(number1, number2));
-
-
-
-
-var substract = function(number1, number2) {
+var subtract = function(number1, number2) {
   return number1 - number2;
 };
-
-
-var number1 = parseInt (prompt("Enter a number:"));
-var number2 = parseInt (prompt("Enter another number:"));
-
-alert(substract(number1, number2));
 
 var multiply = function(number1, number2) {
   return number1 * number2;
 };
 
-
-var number1 = parseInt (prompt("Enter a number:"));
-var number2 = parseInt (prompt("Enter another number:"));
-
-alert(multiply(number1, number2));
-
-var division = function(number1, number2) {
+var divide = function(number1, number2) {
   return number1 / number2;
 }
 
-var number1 = parseInt (prompt("Enter a number"));
-var number2 = parseInt (prompt ("Enter another number"));
-
-alert (division(number1, number2));
+//UI logic
+$(document).ready(function() {
+  $("form#add").submit(function(event) {
+    event.preventDefault();
+    var result = add(parseInt($("#add1").val()), parseInt($("#add2").val()));
+    $("#output").text(result);
+  });
+});
